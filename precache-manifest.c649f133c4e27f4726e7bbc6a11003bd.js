1b3bfd0e35bf111b8e927cff681811db2f516e79
Solaire of Astora <zoxon@users.noreply.github.com>
2019-12-30 16:05:23 +0600

    Updates

---
+++ precache-manifest.c649f133c4e27f4726e7bbc6a11003bd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2557564d326e6e1a6da9dc21ba6f97e4",
+    "url": "/react-carousel/index.html"
+  },
+  {
+    "revision": "144ee11452c8088d7620",
+    "url": "/react-carousel/static/css/2.b5ac8a42.chunk.css"
+  },
+  {
+    "revision": "b65dc7576dd2274f2885",
+    "url": "/react-carousel/static/css/main.372d17fd.chunk.css"
+  },
+  {
+    "revision": "144ee11452c8088d7620",
+    "url": "/react-carousel/static/js/2.704c03a3.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-carousel/static/js/2.704c03a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b65dc7576dd2274f2885",
+    "url": "/react-carousel/static/js/main.e64a9e58.chunk.js"
+  },
+  {
+    "revision": "2dea44c2a158870b16e6",
+    "url": "/react-carousel/static/js/runtime-main.d6cc3268.js"
+  },
+  {
+    "revision": "420298fd64b10013ef246e48f753fbdd",
+    "url": "/react-carousel/static/media/arrow-left.420298fd.svg"
+  },
+  {
+    "revision": "acc086627083522cd5f10cfc9df3cbcb",
+    "url": "/react-carousel/static/media/arrow-right.acc08662.svg"
+  },
+  {
+    "revision": "66321c50191c8a4ccc7af10c80e09b76",
+    "url": "/react-carousel/static/media/image1.66321c50.jpg"
+  },
+  {
+    "revision": "072a19c429995febb12644daea45ec7d",
+    "url": "/react-carousel/static/media/image2.072a19c4.jpg"
+  },
+  {
+    "revision": "1251366fdd53c2cbee85a7e559a189cf",
+    "url": "/react-carousel/static/media/image3.1251366f.jpg"
+  },
+  {
+    "revision": "d63c6397fb7df949b49a7685acf3e071",
+    "url": "/react-carousel/static/media/image4.d63c6397.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-carousel/precache-manifest.40c46f5b2b756fa919bc7aba8d4c78af.js"
+  "/react-carousel/precache-manifest.c649f133c4e27f4726e7bbc6a11003bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-carousel/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
